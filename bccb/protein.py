83dc0ccc380a47f2c6a83bc12307329ed1d1439c
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2022-10-09 02:37:47 +0300

    add build function for nodes and edges

--- bccb/protein.py
+++ bccb/protein.py
@@ -597,13 +597,19 @@ class Uniprot_data:
 
         return accession, secondary_accessions, length, mass, tax_id, organism, protein_names, chromosome_ids, chromosome_info,\
                entrez_id, ec_numbers, kegg, ensembl_transcript, ensemble_gene_ids, genes, virus_hosts_tax_ids
-
-    def call_biocypher_adapter(self, early_stopping=None):
+    
+    def build_nodes_and_edges(self, early_stopping=None):
+        # generate nodes and edges
+        protein_nodes, gene_nodes, organism_nodes, gene_to_protein_edges, protein_to_organism_edges = self.generate_nodes_and_edges(data, early_stopping=early_stopping)
+        
+        self.nodes = protein_nodes + gene_nodes + organism_nodes
+        self.edges = gene_to_protein_edges + protein_to_organism_edges
+        
+    def call_biocypher_adapter(self):
         """
         if "early_stopping" is specified with an integer value, it stops preprocessing on the specified row.
         """
 
         print("Calling Biocypher adapter")
         adapt = BiocypherAdapter(offline=True, db_name="neo4j", wipe=True, quote_char="'")
-        adapt.build_python_object(data=self.uniprot_df, early_stopping=early_stopping)
-        adapt.write_to_csv_for_admin_import()
+        adapt.write_to_csv_for_admin_import(nodes=self.nodes, edges=self.edges)
