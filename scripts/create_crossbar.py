b8ed8ae28d77c245a696e99eead117801a61d580
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2022-10-09 02:46:07 +0300

    add build_nodes_and_edges function

--- scripts/create_crossbar.py
+++ scripts/create_crossbar.py
@@ -11,4 +11,5 @@ from bccb.protein import Uniprot_data
 uniprot_data =  Uniprot_data()
 uniprot_data.uniprot_data_download()
 uniprot_data.build_dataframe()
-uniprot_data.call_biocypher_adapter(early_stopping=5000) # if you want to process whole dataset make early_stopping None
+uniprot_data.build_nodes_and_edges(early_stopping=5000) # if you want to process whole dataset make early_stopping None
+uniprot_data.call_biocypher_adapter() 
