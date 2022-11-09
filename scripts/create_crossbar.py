a06f09d7c397f5767ea66e495cea56aaa68f5fa6
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2022-11-09 15:32:01 +0100

    rename

--- scripts/create_crossbar.py
+++ scripts/create_crossbar.py
@@ -2,13 +2,9 @@
 CROssBAR generation through BioCypher script
 """
 
-# import sys
-# sys.path.append('') # fix weird poetry behaviour I don't understand
-# may not be necessary depending on your setup
-
 from bccb.uniprot_adapter import Uniprot
 
-uniprot_data = Uniprot()
-uniprot_data.uniprot_data_download(cache=True)
-uniprot_data.write_uniprot_nodes_and_edges()
+uniprot_adapter = Uniprot()
+uniprot_adapter.uniprot_data_download(cache=True)
+uniprot_adapter.write_uniprot_nodes_and_edges()
 
