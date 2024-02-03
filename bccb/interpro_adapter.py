36d4dcf62a3f74321f792dc5398d601a05dc8d44
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2024-02-03 21:48:28 +0300

    `interpro_adapter`: update
    - add `__contains__` magic function to enums 
    - add case insensitivity when looking enum values 
    - black formatting 
    - some small refactoring

--- bccb/interpro_adapter.py
+++ bccb/interpro_adapter.py
@@ -16,68 +16,96 @@ from biocypher._logger import logger
 from typing import Literal, Union, Optional
 from pydantic import BaseModel, DirectoryPath, validate_call
 
-from enum import Enum
+from enum import Enum, EnumMeta
 
 logger.debug(f"Loading module {__name__}.")
 
-class InterProNodeField(Enum):
+
+class InterProEnumMeta(EnumMeta):
+    def __contains__(cls, item):
+        return item in cls.__members__.keys()
+
+
+class InterProNodeField(Enum, metaclass=InterProEnumMeta):
     """
-    Domain node fields in InterPro 
+    Domain node fields in InterPro
     """
-    
+
     # primary attributes
     PROTEIN_COUNT = "protein_count"
     NAME = "name"
     TYPE = "type"
     PARENT_LIST = "parent_list"
     CHILD_LIST = "child_list"
-    
+
     # member list attributes
     PFAM = "PFAM"
-    
+
     # external attributes
     EC = "EC"
-    
+
     # structural attributes
     PDB = "PDB"
-    
+
+    @classmethod
+    def _missing_(cls, value: str):
+        value = value.lower()
+        for member in cls.__members__.values():
+            if member.value.lower() == value:
+                return member
+        return None
+
     @classmethod
     def get_primary_attributes(cls):
         """
         Returns primary InterPro attributes
         """
-        return [cls.PROTEIN_COUNT.value, cls.NAME.value, cls.TYPE.value,
-               cls.PARENT_LIST.value, cls.CHILD_LIST.value]
-    
+        return [
+            cls.PROTEIN_COUNT.value,
+            cls.NAME.value,
+            cls.TYPE.value,
+            cls.PARENT_LIST.value,
+            cls.CHILD_LIST.value,
+        ]
+
     @classmethod
     def get_member_list_attributes(cls):
         """
         Returns external InterPro attributes
         """
         return [cls.PFAM.value]
-    
+
     @classmethod
     def get_external_attributes(cls):
         """
         Returns external InterPro attributes
         """
         return [cls.EC.value]
-    
+
     @classmethod
     def get_structural_attributes(cls):
         """
         Returns structural InterPro attributes
         """
         return [cls.PDB.value]
-    
-    
-class InterProEdgeField(Enum):
+
+
+class InterProEdgeField(Enum, metaclass=InterProEnumMeta):
     """
-    Domain edge fields in InterPro 
+    Domain edge fields in InterPro
     """
+
     START = "start"
     END = "end"
-    
+
+    @classmethod
+    def _missing_(cls, value: str):
+        value = value.lower()
+        for member in cls.__members__.values():
+            if member.value.lower() == value:
+                return member
+        return None
+
 
 class InterProModel(BaseModel):
     cache: bool = False
@@ -90,23 +118,26 @@ class InterProModel(BaseModel):
     edge_fields: Union[list[InterProEdgeField], None] = None
     test_mode: bool = False
 
+
 class InterPro:
     """
     Class that downloads InterPro data using pypath and reformats it to be ready
     for import into a BioCypher database.
     """
 
-    def __init__(self, 
-                 cache: Optional[bool] = False, 
-                 debug: Optional[bool] = False, 
-                 page_size: Optional[int] = 150, 
-                 retries: Optional[int] = 6, 
-                 organism: Optional[int | Literal["*"] | None] = None, 
-                 add_prefix: Optional[bool]= True,
-                 node_fields: Optional[Union[list[InterProNodeField], None]] = None, 
-                 edge_fields: Optional[Union[list[InterProEdgeField], None]] = None,
-                 test_mode: Optional[bool]= False):
-        """        
+    def __init__(
+        self,
+        cache: Optional[bool] = False,
+        debug: Optional[bool] = False,
+        page_size: Optional[int] = 150,
+        retries: Optional[int] = 6,
+        organism: Optional[int | Literal["*"] | None] = None,
+        add_prefix: Optional[bool] = True,
+        node_fields: Optional[Union[list[InterProNodeField], None]] = None,
+        edge_fields: Optional[Union[list[InterProEdgeField], None]] = None,
+        test_mode: Optional[bool] = False,
+    ):
+        """
         Args:
             cache: if True, it uses the cached version of the data, otherwise
             forces download.
@@ -120,37 +151,41 @@ class InterPro:
             test_mode: limits amount of data for testing
         """
 
-        model = InterProModel(cache=cache,
-                              debug=debug,
-                              retries=retries,
-                              page_size=page_size,
-                              organism=organism,
-                              add_prefix=add_prefix,
-                              node_fields=node_fields,
-                              edge_fields=edge_fields,
-                              test_mode=test_mode).model_dump()
-        
+        model = InterProModel(
+            cache=cache,
+            debug=debug,
+            retries=retries,
+            page_size=page_size,
+            organism=organism,
+            add_prefix=add_prefix,
+            node_fields=node_fields,
+            edge_fields=edge_fields,
+            test_mode=test_mode,
+        ).model_dump()
+
         self.cache = model["cache"]
         self.debug = model["debug"]
         self.retries = model["retries"]
         self.page_size = model["page_size"]
-        self.organism = None if model["organism"] in ("*", None) else model["organism"]
+        self.organism = (
+            None if model["organism"] in ("*", None) else model["organism"]
+        )
         self.add_prefix = model["add_prefix"]
-        
-        
+
         # set node and edge fields
-        self.set_node_and_edge_fields(node_fields=model["node_fields"], edge_fields=model["edge_fields"])
-        
+        self.set_node_and_edge_fields(
+            node_fields=model["node_fields"], edge_fields=model["edge_fields"]
+        )
+
         self.early_stopping = None
         if model["test_mode"]:
-            self.early_stopping = 100        
-
+            self.early_stopping = 100
 
     def download_domain_node_data(self) -> None:
         """
         Downloads domain node data from Interpro
         """
-        
+
         # stack pypath context managers
         with ExitStack() as stack:
 
@@ -161,23 +196,31 @@ class InterPro:
 
             if not self.cache:
                 stack.enter_context(curl.cache_off())
-            
+
             logger.debug("Started downloading InterPro domain data")
             t0 = time()
-            
+
             # To do: Filter according to tax id??
-            self.interpro_entries = interpro.interpro_entries() # returns a list of namedtuples
-            self.interpro_structural_xrefs = interpro.interpro_xrefs(db_type = 'structural')
-            self.interpro_external_xrefs = interpro.interpro_xrefs(db_type = 'external')
-            
+            self.interpro_entries = (
+                interpro.interpro_entries()
+            )  # returns a list of namedtuples
+            self.interpro_structural_xrefs = interpro.interpro_xrefs(
+                db_type="structural"
+            )
+            self.interpro_external_xrefs = interpro.interpro_xrefs(
+                db_type="external"
+            )
+
             t1 = time()
-            logger.info(f'InterPro domain data is downloaded in {round((t1-t0) / 60, 2)} mins')
-            
+            logger.info(
+                f"InterPro domain data is downloaded in {round((t1-t0) / 60, 2)} mins"
+            )
+
     def download_domain_edge_data(self) -> None:
         """
         Downloads Uniprot annotation data from Interpro
         """
-        
+
         with ExitStack() as stack:
 
             stack.enter_context(settings.context(retries=self.retries))
@@ -187,18 +230,26 @@ class InterPro:
 
             if not self.cache:
                 stack.enter_context(curl.cache_off())
-            
+
             logger.debug("Started downloading InterPro annotation data")
             t0 = time()
-            
-            if self.organism:                
+
+            if self.organism:
                 # WARNING: decrease page_size parameter if there is a curl error about timeout in the pypath_log
-                self.interpro_annotations = interpro.interpro_annotations(page_size = self.page_size, reviewed = True, tax_id = self.organism)
+                self.interpro_annotations = interpro.interpro_annotations(
+                    page_size=self.page_size,
+                    reviewed=True,
+                    tax_id=self.organism,
+                )
             else:
-                self.interpro_annotations = interpro.interpro_annotations(page_size = self.page_size, reviewed = True, tax_id = '')
-            
+                self.interpro_annotations = interpro.interpro_annotations(
+                    page_size=self.page_size, reviewed=True, tax_id=""
+                )
+
             t1 = time()
-            logger.info(f'InterPro annotation data is downloaded in {round((t1-t0) / 60, 2)} mins')
+            logger.info(
+                f"InterPro annotation data is downloaded in {round((t1-t0) / 60, 2)} mins"
+            )
 
     @validate_call
     def get_interpro_nodes(self, node_label: str = "domain") -> list[tuple]:
@@ -219,57 +270,91 @@ class InterPro:
         member_list_attributes = InterProNodeField.get_member_list_attributes()
         external_attributes = InterProNodeField.get_external_attributes()
         structural_attributes = InterProNodeField.get_structural_attributes()
-        
+
         logger.debug("Creating domain nodes")
         t0 = time()
-        
+
         # set counter for early stopping
         counter = 0
-        
+
         for entry in tqdm(self.interpro_entries):
-            props = dict()
+            props = {}
             interpro_props = entry._asdict()
-            
+
             domain_id = self.add_prefix_to_id("interpro", entry.interpro_id)
-            
+
             # get primary InterPro attributes
             for element in primary_attributes:
-                
+
                 if element in self.node_fields and interpro_props.get(element):
                     if element == "protein_count":
-                        props[element.replace(" ", "_").lower()] = int(interpro_props.get(element))
+                        props[element.replace(" ", "_").lower()] = int(
+                            interpro_props.get(element)
+                        )
                     else:
-                        props[element.replace(" ", "_").lower()] = self.check_length(interpro_props.get(element))
-            
+                        props[element.replace(" ", "_").lower()] = (
+                            self.check_length(interpro_props.get(element))
+                        )
+
             # get member list InterPro attributes
             for element in member_list_attributes:
-                if element in self.node_fields and interpro_props.get('member_list').get(element):
-                    props[element.replace(" ", "_").lower()] = self.check_length(interpro_props.get('member_list').get(element))
-                        
-            # get external InterPro attributes            
+                if element in self.node_fields and interpro_props.get(
+                    "member_list"
+                ).get(element):
+                    props[element.replace(" ", "_").lower()] = (
+                        self.check_length(
+                            interpro_props.get("member_list").get(element)
+                        )
+                    )
+
+            # get external InterPro attributes
             for element in external_attributes:
-                if element in self.node_fields and self.interpro_external_xrefs.get(entry.interpro_id).get(element):
-                    props[element.replace(" ", "_").lower()] = self.check_length(self.interpro_external_xrefs.get(entry.interpro_id).get(element))
-            
+                if (
+                    element in self.node_fields
+                    and self.interpro_external_xrefs.get(entry.interpro_id).get(
+                        element
+                    )
+                ):
+                    props[element.replace(" ", "_").lower()] = (
+                        self.check_length(
+                            self.interpro_external_xrefs.get(
+                                entry.interpro_id
+                            ).get(element)
+                        )
+                    )
+
             # get structural InterPro attributes
             for element in structural_attributes:
-                    if element in self.node_fields and self.interpro_structural_xrefs.get(entry.interpro_id).get(element):
-                        props[element.replace(" ", "_").lower()] = self.check_length(self.interpro_structural_xrefs.get(entry.interpro_id).get(element))
-              
+                if (
+                    element in self.node_fields
+                    and self.interpro_structural_xrefs.get(
+                        entry.interpro_id
+                    ).get(element)
+                ):
+                    props[element.replace(" ", "_").lower()] = (
+                        self.check_length(
+                            self.interpro_structural_xrefs.get(
+                                entry.interpro_id
+                            ).get(element)
+                        )
+                    )
+
             node_list.append((domain_id, node_label, props))
-            
+
             counter += 1
-            
+
             if self.early_stopping and counter == self.early_stopping:
                 break
-        
+
         t1 = time()
-        logger.info(f'InterPro nodes created in {round((t1-t0) / 60, 2)} mins')
+        logger.info(f"InterPro nodes created in {round((t1-t0) / 60, 2)} mins")
 
         return node_list
-    
+
     @validate_call
-    def get_interpro_edges(self, edge_label: str = "protein_has_domain") -> list[tuple]:
+    def get_interpro_edges(
+        self, edge_label: str = "protein_has_domain"
+    ) -> list[tuple]:
         """
         Prepares Protein-Domain edges for BioCypher
         Args:
@@ -278,43 +363,49 @@ class InterPro:
 
         if not hasattr(self, "interpro_annotations"):
             self.download_domain_edge_data()
-        
+
         # create list of edges
         edge_list = []
-        
+
         logger.debug("Creating protein-domain edges")
         t0 = time()
-        
+
         # set counter for early stopping
         counter = 0
 
         # DOMAIN-PROTEIN EDGES
-        for k, v in tqdm(self.interpro_annotations.items()):            
+        for k, v in tqdm(self.interpro_annotations.items()):
             # k -> uniprot id
             for annotation in v:
-                
+
                 interpro_props = annotation._asdict()
-                props = dict()
-                
+                props = {}
+
                 for field in self.edge_fields:
                     if interpro_props.get(field, None):
-                        props[field.replace(" ","_").lower()] = self.check_length(interpro_props[field])
-                    
-                interpro_id = self.add_prefix_to_id("interpro", annotation.interpro_id)
+                        props[field.replace(" ", "_").lower()] = (
+                            self.check_length(interpro_props[field])
+                        )
+
+                interpro_id = self.add_prefix_to_id(
+                    "interpro", annotation.interpro_id
+                )
                 uniprot_id = self.add_prefix_to_id("uniprot", k)
-                
-                edge_list.append((None, uniprot_id, interpro_id, edge_label, props))
-                
+
+                edge_list.append(
+                    (None, uniprot_id, interpro_id, edge_label, props)
+                )
+
                 counter += 1
-                
+
             if self.early_stopping and counter >= self.early_stopping:
                 break
-                
+
         t1 = time()
-        logger.info(f'InterPro edges created in {round((t1-t0) / 60, 2)} mins')
-        
+        logger.info(f"InterPro edges created in {round((t1-t0) / 60, 2)} mins")
+
         return edge_list
-    
+
     @validate_call
     def check_length(self, element: str | list) -> str | list:
         """
@@ -324,33 +415,34 @@ class InterPro:
             return element[0]
         else:
             return element
-    
+
     @validate_call
-    def add_prefix_to_id(self, prefix: str = None, identifier: str = None, sep: str = ":") -> str:
+    def add_prefix_to_id(
+        self, prefix: str = None, identifier: str = None, sep: str = ":"
+    ) -> str:
         """
         Adds prefix to ids
         """
         if self.add_prefix:
-            return normalize_curie(prefix + sep + str(identifier))
-        
-        return identifier      
-        
+            return normalize_curie(prefix + sep + identifier)
+
+        return identifier
+
     def set_node_and_edge_fields(self, node_fields, edge_fields) -> None:
         """
-        Sets Interpro node and edge fields 
+        Sets Interpro node and edge fields
         """
-        
+
         if node_fields:
-            self.node_fields = [field.value for field in node_fields]            
+            self.node_fields = [field.value for field in node_fields]
         else:
             self.node_fields = [field.value for field in InterProNodeField]
-            
+
         if edge_fields:
             self.edge_fields = [field.value for field in edge_fields]
         else:
             self.edge_fields = [field.value for field in InterProEdgeField]
 
-    
     def export_as_csv(self, path: DirectoryPath | None = None):
         if path:
             node_full_path = os.path.join(path, "Domain.csv")
@@ -363,10 +455,9 @@ class InterPro:
         nodes = self.get_interpro_nodes()
         node_df_list = []
         for n in nodes:
-                props = {}
-                props["id"] = n[0]
-                props = props | n[2]
-                node_df_list.append(props)
+            props = {"id": n[0]}
+            props |= n[2]
+            node_df_list.append(props)
 
         nodes_df = pd.DataFrame.from_records(node_df_list)
         nodes_df.to_csv(node_full_path, index=False)
@@ -376,13 +467,10 @@ class InterPro:
         edges = self.get_interpro_edges()
         edges_df_list = []
         for e in edges:
-            props = {}
-            props["source_id"] = e[1]
-            props["target_id"] = e[2]
-            props = props | e[4]
+            props = {"source_id": e[1], "target_id": e[2]}
+            props |= e[4]
             edges_df_list.append(props)
 
         edges_df = pd.DataFrame.from_records(edges_df_list)
         edges_df.to_csv(edge_full_path, index=False)
         logger.info(f"Domain edge data is written: {node_full_path}")
-        
